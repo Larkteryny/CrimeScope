ccd3d397dfa11f5b24b5721b78827d86820b2ace
Larkteryny <103611388+Larkteryny@users.noreply.github.com>
2025-01-26 01:35:44 -0500

    Add tab icon and fix single report circle

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 // Setup sidebar
-window.onload = function(){
-    const sidebar = document.querySelector(".sidebar");
-    const closeBtn = document.querySelector("#btn");
+window.onload = function () {
+  const sidebar = document.querySelector(".sidebar");
+  const closeBtn = document.querySelector("#btn");
 
-    // sidebar click part
-    closeBtn.addEventListener("click",function(){
-        sidebar.classList.toggle("open")
-        menuBtnChange()
-    })
+  // sidebar click part
+  closeBtn.addEventListener("click", function () {
+    sidebar.classList.toggle("open")
+    menuBtnChange()
+  })
 
-    // menu button changer part
-    function menuBtnChange(){
-        if(sidebar.classList.contains("open")){
-            closeBtn.classList.replace("bx-menu","bx-menu-alt-right")
-        }else{
-            closeBtn.classList.replace("bx-menu-alt-right","bx-menu")
-        }
+  // menu button changer part
+  function menuBtnChange() {
+    if (sidebar.classList.contains("open")) {
+      closeBtn.classList.replace("bx-menu", "bx-menu-alt-right")
+    } else {
+      closeBtn.classList.replace("bx-menu-alt-right", "bx-menu")
     }
+  }
 }
 
 
@@ -100,89 +100,96 @@ async function read_data() {
 }
 
 var color_grads = new Map([
-    ["Assault", ["FF6464", "FF1212"]],
-    ["Break and Enter", ["66B2FF", "007BFF"]],
-    ["Auto Theft", ["FFC966", "FFA500"]],
-    ["Theft Over", ["85D788", "28A745"]],
-    ["Robbery", ["B085F5", "6F42C1"]],
+  ["Assault", ["FF6464", "FF1212"]],
+  ["Break and Enter", ["66B2FF", "007BFF"]],
+  ["Auto Theft", ["FFC966", "FFA500"]],
+  ["Theft Over", ["85D788", "28A745"]],
+  ["Robbery", ["B085F5", "6F42C1"]],
 ]);
 function color_interpol(start, end, n) {
-    var r0 = parseInt(start.substring(0, 2), 16), rd = parseInt(end.substring(0, 2), 16) - r0;
-    var g0 = parseInt(start.substring(2, 4), 16), gd = parseInt(end.substring(2, 4), 16) - g0;
-    var b0 = parseInt(start.substring(4, 6), 16), bd = parseInt(end.substring(4, 6), 16) - b0;
-    var scale = (1 / (0.9 + n/10))  // 
-    var r = parseInt(r0 + rd * scale), g = parseInt(g0 + gd * scale), b = parseInt(b0 + bd * scale);
-    
-    return '#' + r.toString(16).padStart(2, '0') + g.toString(16).padStart(2, '0') + b.toString(16).padStart(2, '0') + '88'
+  var r0 = parseInt(start.substring(0, 2), 16), rd = parseInt(end.substring(0, 2), 16) - r0;
+  var g0 = parseInt(start.substring(2, 4), 16), gd = parseInt(end.substring(2, 4), 16) - g0;
+  var b0 = parseInt(start.substring(4, 6), 16), bd = parseInt(end.substring(4, 6), 16) - b0;
+  var scale = (1 / (0.9 + n / 10))  // 
+  var r = parseInt(r0 + rd * scale), g = parseInt(g0 + gd * scale), b = parseInt(b0 + bd * scale);
+
+  return '#' + r.toString(16).padStart(2, '0') + g.toString(16).padStart(2, '0') + b.toString(16).padStart(2, '0') + '88'
 }
 
 var activated = new Map([
-    ["Assault", true],
-    ["Break and Enter", true],
-    ["Auto Theft", true],
-    ["Theft Over", true],
-    ["Robbery", true],
+  ["Assault", true],
+  ["Break and Enter", true],
+  ["Auto Theft", true],
+  ["Theft Over", true],
+  ["Robbery", true],
 ]);
 var clusterGroups = new Map();
 function create_clusters() {
-    accidents.forEach((records, category) => {
-        var clusterGroup = L.markerClusterGroup({
-            iconCreateFunction: function (cluster) {
-                var markers = cluster.getAllChildMarkers();
-                var n = markers.length;
-                var size = 30 + Math.sqrt(n) * 5;
-                var color = color_interpol(color_grads.get(category)[0], color_grads.get(category)[1], n);
-                return L.divIcon({
-                    html: `<div style="width: ${size}px; 
-                    height: ${size}px; 
-                    background-color: ${color}; 
-                    text-align: center; 
-                    font-size: 24px;
-                    border-radius: ${size}px;"></div>`,
-                    className: '',
-                    iconSize: L.point(size, size),
-                });
-            },
-            chunkedLoading: true,
+  accidents.forEach((records, category) => {
+    var clusterGroup = L.markerClusterGroup({
+      iconCreateFunction: function (cluster) {
+        var markers = cluster.getAllChildMarkers();
+        var n = markers.length;
+        var size = 30 + Math.sqrt(n) * 5;
+        var color = color_interpol(color_grads.get(category)[0], color_grads.get(category)[1], n);
+        return L.divIcon({
+          html: `<div style="width: ${size}px; 
+                  height: ${size}px; 
+                  background-color: ${color}; 
+                  text-align: center; 
+                  font-size: 24px;
+                  border-radius: ${size}px;"></div>`,
+          className: '',
+          iconSize: L.point(size, size),
         });
-        clusterGroups.set(category, clusterGroup);
-
-        for (marker of records) {
-            clusterGroup.addLayer(L.circle([marker[0], marker[1]], {
-                color: '#' + color_grads.get(category)[0],
-                fillColor: '#' + color_grads.get(category)[0],
-                fillOpacity: 0.5,
-                radius: 100,
-            }));
-        }
-        map.addLayer(clusterGroup);
+      },
+      chunkedLoading: true,
     });
+    clusterGroups.set(category, clusterGroup);
+
+    myIcon = L.divIcon({
+      html: `<div style="width: 20px; 
+          height: 20px; 
+          background-color: #${color_grads.get(category)[1]}88; 
+          text-align: center; 
+          font-size: 24px;
+          border-radius: 20px;"></div>`,
+      className: '',
+      iconSize: L.point(20, 20),
+    });
+    for (marker of records) {
+      clusterGroup.addLayer(L.marker([marker[0], marker[1]], {
+        icon: myIcon,
+      }));
+    }
+    map.addLayer(clusterGroup);
+  });
 }
 
 function toggleFilter(category) {
-    activated.set(category, !activated.get(category));
-    if (activated.get(category)) {
-        map.addLayer(clusterGroups.get(category))
-    } else {
-        map.removeLayer(clusterGroups.get(category));
-    }
+  activated.set(category, !activated.get(category));
+  if (activated.get(category)) {
+    map.addLayer(clusterGroups.get(category))
+  } else {
+    map.removeLayer(clusterGroups.get(category));
+  }
 }
 
 async function main() {
-    accidents = await read_data();
-    if(accidents == null) {
-        console.error("Nothing returned from read_data()");
-    }
+  accidents = await read_data();
+  if (accidents == null) {
+    console.error("Nothing returned from read_data()");
+  }
 
-    create_clusters();
+  create_clusters();
 }
 
 // Initialize Leaflet map
 var map = L.map('map').setView([43.733644, -79.361635], 11);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    maxNativeZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  maxZoom: 19,
+  maxNativeZoom: 19,
+  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
 main()
